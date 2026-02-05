69883d2f566750295c7d409e0d306d7338ddef3d
csc23 <csc23@skbl-418-009.ph.ic.ac.uk>
2026-02-05 09:36:13 +0000

    Changed PORTB to PORTC 2nd time

--- main.s
+++ main.s
@@ -9,7 +9,7 @@ main:
 	org	0x100		    ; Main code starts here at address 0x100
 start:
 	movlw 	0x0
-	movwf	TRISB, A	    ; Port C all outputs
+	movwf	TRISC, A	    ; Port C all outputs
 	bra 	test
 loop:
 	movff 	0x06, PORTC
