ff09b8bddd06949f8a92e0e728d681c87b5b1e06
csc23 <csc23@skbl-418-009.ph.ic.ac.uk>
2026-02-12 09:52:03 +0000

    Count up down

--- main.s
+++ main.s
@@ -1,4 +1,4 @@
-	#include <xc.inc>
+#include <xc.inc>
 
 psect	code, abs
 	
@@ -6,21 +6,34 @@ main:
 	org	0x0
 	goto	start
 
-	org	0x100		    ; Main code starts here at address 0x100
+	org	0x100		    ; Main code starts here
 start:
-	movlw	0xFF
-	movwf	TRISD, A
-	movlw 	0x0
-	movwf	TRISC, A	    ; Port C all outputs
-	bra 	test
-loop:
-	movff 	0x06, PORTC
-	incf 	0x06, W, A
-test:
-	movwf	0x06, A	    ; Test for end of loop condition
-	movf	PORTD, W, A
-	cpfsgt 	0x06, A
-	bra 	loop		    ; Not yet finished goto start of loop again
-	goto 	0x0		    ; Re-run program from start
+	clrf	0x06, A	    ; Counter = 0
+	movlw 	0x00
+	movwf	TRISD, A	; Port D all outputs
+	movwf	TRISC, A	; Port C all outputs
+	bra 	test_up
+
+count_up:
+	movff 	0x06, PORTC	; Output counter to PORTC
+	incf	0x06, F, A	; Increment counter (store back in file!)
+	
+test_up:
+	movlw   0x63		; 99 decimal
+	cpfseq 	0x06, A		; Skip next if 0x06 == 0x63
+	bra 	count_up	; Not 99 yet ? keep counting up
+	bra 	count_down	; Reached 99 ? start counting down
+
+
+count_down:
+	movff 	0x06, PORTC	; Output counter
+	decf 	0x06, F, A	; Decrement counter
+	
+test_down:
+	movlw   0x00
+	cpfseq 	0x06, A		; Skip if counter == 0
+	bra 	count_down	; Not zero yet ? keep counting down
+	goto 	start		; Reached 0 ? restart whole cycle
+
 
 	end	main
